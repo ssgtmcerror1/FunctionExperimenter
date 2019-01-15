1d254f3f02f722b554b4979ef57672bea72e4887
David Walsh <ssgtmcerror1@gmail.com>
2019-01-15 12:45:59 -0500

    Add files via upload

--- Experiment.py
+++ Experiment.py
@@ -1,3 +1,4 @@
+import csv
 # FunctionExperimenter class
 # Andrew Penland and David Walsh, 2019
 
@@ -8,11 +9,11 @@ class Experiment:
 
     # constructor
     def __init__(self, function, initial_values, orbit_length, var_list, file_to_write):
-        self.function = function
-        self.init_values = initial_values.copy()
-        self.orbit_length = orbit_length
-        self.var_list = var_list.copy()
-        self.file_to_write = file_to_write
+        self._function = function
+        self._init_values = initial_values.copy()
+        self._orbit_length = orbit_length
+        self._var_list = var_list.copy()
+        self._file_to_write = file_to_write
 
     def orbit(self, n):
         # should return x0, x1, x2, ..., xn
@@ -28,12 +29,18 @@ class Experiment:
 
     def write_csv(self):
         #write the csv file with results
-        pass
+        with open(self._file_to_write, 'w', newline='') as csv_file:
+            csv_writer = csv.writer(csv_file, delimiter=',')
+            #write column headers
+            csv_writer.writerow(['1','2','3','4'])
+            #write rows
+            print('Successfully saved CSV!')
 
 
 new_function = Experiment("x+1", ['1', '2', '3'], 3, ['a', 'b', 'c'], "somefile.csv")
-print(new_function.function)
-print(new_function.init_values)
-print(new_function.orbit_length)
-print(new_function.var_list)
-print(new_function.file_to_write)
+print(new_function._function)
+print(new_function._init_values)
+print(new_function._orbit_length)
+print(new_function._var_list)
+print(new_function._file_to_write)
+new_function.write_csv()
