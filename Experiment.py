26bc89e8bed61e36fa829ff932f51a48242b9105
David Walsh <ssgtmcerror1@gmail.com>
2019-01-21 12:56:05 -0500

    Add files via upload

--- Experiment.py
+++ Experiment.py
@@ -1,4 +1,6 @@
 import csv
+import timeit
+import string
 
 # FunctionExperimenter class
 # Andrew Penland and David Walsh, 2019
@@ -11,63 +13,115 @@ class Experiment:
     # constructor
     def __init__(self, function, initial_values, orbit_length, var_list, file_to_write):
         self._evaluate_function = function
-        self._init_values = initial_values.copy()
-        self._orbit_length = orbit_length
-        self._var_list = var_list.copy()
+        self._function_name = function.__name__
         self._file_to_write = file_to_write
+        self._init_values = initial_values.copy()
+        self._orbit_length = orbit_length+1
+        self._var_list = var_list.copy()
+        self._results = []
+        self._function_calculation_time = 0
 
-    def orbit(self, n):
-        # should return x0, x1, x2, ..., xn
-        # x_{i+1} = f(x_i)
-        pass
+    @staticmethod
+    def chunk_list(result_list, chunk_size):
+        for i in range(0, len(result_list), chunk_size):
+            yield result_list[i:i + chunk_size]
 
-    # one major method: run the experiment
-    def run(self):
-        # make the function repeat function on each initial value
-        # self.apply_function_orbit()
-        # record each var in var_list to file_to_write
-        # write the file list
-        self.write_csv()
-        pass
+    def apply_function_orbit(self):
+        results = []
 
-    # writes the csv file with results
-    def write_csv(self):
-        # open the file
-        file = open(self._file_to_write, 'w')
-        with file:
-            writer = csv.writer(file)
-            # generate the headers
-            header = self.generate_csv_headers()
-            writer.writerow(header)
+        for value in self._init_values:
+            orbit = 0
+            current_value = float(value)
+            while orbit <= self._orbit_length-1:
+                result = self._evaluate_function(current_value+orbit)
+                results.append(result)
+                orbit += 1
+
+        return results
 
     # generate a list of column headers with function and var list
     def generate_csv_headers(self, name):
-        # empty list to store column header names
         column_headers = []
+        length_of_var_list = len(self._var_list)
 
-        # iterate through orbit length and append function_name + current orbit value as a string
         for i in range(0, self._orbit_length):
             column_headers.append(name + '[' + str(i) + ']')
 
-        # get length of var_list
-        length_of_var_list = len(self._var_list)
         for j in range(0, length_of_var_list):
             column_headers.append(self._var_list[j])
 
         return column_headers
 
-    def apply_function_orbit(self):
+    def orbit_headers(self):
+        # should return x0, x1, x2, ..., xn
+        # x_{i+1} = f(x_i)
 
-        results = []
-        current_result_values = []
-        for i in range(0, len(self._init_values)):
-            for j in range(0, self._orbit_length-1):
-                # get the result of the current value applied to the function
-                result = self._evaluate_function(int(self._init_values[i]) + j)
-                # append result to the results_values list
-                current_result_values.append(result)
-                # append complete list of results to the list of results
-        results.append(current_result_values)
+        orbit_header_string = []
+        for i in range(0, self._orbit_length):
+            orbit_header_string.append('x_{i+' + str(i) + '}')
 
-        print(results)
+        return orbit_header_string
+
+    # one major method: run the experiment
+    def run(self):
+        # record start time
+        start_time = timeit.default_timer()
+
+        # make the function repeat function on each initial value
+        self._results = self.apply_function_orbit()
+
+        # calculate execution time
+        end_time = timeit.default_timer()
+        self._function_calculation_time = (end_time - start_time)
+
+        # DEBUG: print name of the function code passed in
+        print("Function: " + self._function_name)
+        print("=======")
+
+        # DEBUG: print orbit headers
+        orbit_header = self.orbit_headers()
+        print(orbit_header)
+
+        # DEBUG: print function results
+        chunked_results = list(self.chunk_list(self._results, self._orbit_length))
+        index = 0
+
+        for value in self._init_values:
+            print('f(' + value + ')=', end='')
+            print(chunked_results[index])
+            index += 1
+
+        # record each var in var_list to file_to_write
+        # TODO: implement this
+
+        # write the file list
+        self.write_csv()
+
+        # DEBUG: print function calculation time
+        print("=======")
+        print("Function evaluated in %f seconds. \n" % self._function_calculation_time)
+
+    # writes the csv file with results
+    def write_csv(self):
+        file = open(self._file_to_write, 'w', newline='')
+        with file:
+            writer = csv.writer(file)
+
+            # write function name
+            writer.writerow(['Function: ' + self._function_name])
+
+            # orbit headers
+            field_names = self.orbit_headers()
+            writer.writerow([''] + field_names)
+
+            # write results
+            results = list(self.chunk_list(self._results, self._orbit_length))
+            for i in range(0, len(results)):
+                writer.writerow(['f(' + str(self._init_values[i]) + ')'] + results[i])
+
+            # header = self.generate_csv_headers()
+            # writer.writerow(header)
+
+            # function calculation time
+            writer.writerow(['Function evaluated in %f seconds.' % self._function_calculation_time])
 
--- someotherfile.py
+++ someotherfile.py
@@ -4,19 +4,39 @@ from Experiment import *
 def x_plus_one(x):
     return x+1
 
+
+def x_times_ten(x):
+    return x * 10
+
+
 def x_mod_four(x):
     return x % 4
 
-# 2,3,4
-# 3,4,5
-# 4,5,6
+
+def collatz(number):
+    number = int(number)
+    if number % 2 == 0:
+        number = (number//2)
+        return number
+    if number % 2 == 1:
+        number = (3*number+1)
+
+    return number
 
 
-new_function = Experiment(x_plus_one, ['1', '2', '3'], 4, ['a', 'b', 'c'], "somefile.csv")
-y = new_function.apply_function_orbit()
-print(y)
+# new_function = Experiment(collatz, ['1.0', '2.0', '3.0'], 5, ['a', 'b', 'c'], "somefile.csv")
+# new_function.run()
 
-new_function_two = Experiment(x_mod_four, ['1', '2', '3'], 4, ['a', 'b', 'c'], "somefile.csv")
-z = new_function_two.apply_function_orbit()
-print(z)
+new_function_two = Experiment(
+    x_times_ten,  # function code
+    ['30', '22', '11', '19', '16'],  # initial values
+    10,  # orbit length
+    ['a', 'b', 'c'],  # var list
+    "somefile.csv"  # csv file to save results
+    )
+new_function_two.run()
+
+
+# new_function_three = Experiment(x_mod_four, ['1', '2', '3'], 5, ['a', 'b', 'c'], "somefile.csv")
+# new_function_three.run()
 
